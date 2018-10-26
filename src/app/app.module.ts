39e54c695d5b54c41e8471eccaf62b72e5fed569
sylvester omondi <sylvesteromondi49@gmail.com>
2018-10-26 21:17:58 +0300

    generated a landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule
