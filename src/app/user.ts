58a771df55039c24dccd2314afeeb776644a19e9
sylvester omondi <sylvesteromondi49@gmail.com>
2018-10-27 10:49:28 +0300

    indentation and spaciing

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,6 @@
-export class User {
+export class Users {
+    constructor(public Name: string,public html_url: any,public avatar_url,public public_repos: any,public repo_url: any, public created_at:Date) {
+
 }
+}
+
